a28c1a2f20362834dbdc5ecf548293fc30f61e12
Farah-AlBader <70542682+Farah-AlBader@users.noreply.github.com>
2020-10-17 20:03:33 +0300

    not done yet

---
+++ Kitaby/Kitaby/VCs/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Kitaby
+//
+//  Created by Mac on 10/15/20.
+//  Copyright © 2020 :). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Kitaby/Kitaby/VCs/mainVC.swift
@@ -0,0 +1,42 @@
+//
+//  mainVC.swift
+//  Kitaby
+//
+//  Created by Mac on 10/15/20.
+//  Copyright © 2020 :). All rights reserved.
+//
+
+import UIKit
+
+class mainVC: UIViewController {
+
+    @IBOutlet weak var signUpButton: UIButton!
+    @IBOutlet weak var logInButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        signUpButton.layer.cornerRadius = 10
+        logInButton.layer.cornerRadius = 10
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func signUp(_ sender: Any) {
+        performSegue(withIdentifier: "goToSignUp", sender: nil)
+    }
+    
+    @IBAction func logIn(_ sender: Any) {
+        performSegue(withIdentifier: "goToLogIn", sender: nil)
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
