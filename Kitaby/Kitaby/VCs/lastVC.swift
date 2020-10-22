a89dc5a05125e715fc0cee65fb47e9d5f1fc88e0
Farah-AlBader <70542682+Farah-AlBader@users.noreply.github.com>
2020-10-22 20:34:35 +0300

    .

---
+++ Kitaby/Kitaby/VCs/lastVC.swift
@@ -0,0 +1,29 @@
+//
+//  lastVC.swift
+//  FirebaseTemplate
+//
+//  Created by Mac on 10/22/20.
+//  Copyright © 2020 OMAR. All rights reserved.
+//
+
+import UIKit
+
+class lastVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
